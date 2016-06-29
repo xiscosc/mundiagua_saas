89ecf0c8e0092b430084306d8c9ac4d4221697b4
Xisco Sastre <xiscosastre@gmail.com>
2016-06-29 17:19:52 +0200

    favicon

--- intervention/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import forms
-
-from intervention.models import InterventionModification
-
-
-class ModificationForm(forms.ModelForm):
-    class Meta:
-        model = InterventionModification
-        fields = ["note"]
