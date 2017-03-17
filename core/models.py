4bc0db67cd2afdfc1cb8ec1ff2ac62b3c8510f7d
Xisco Sastre <xiscosastre@gmail.com>
2017-03-17 16:10:14 +0100

    password change

--- core/models.py
+++ core/models.py
@@ -51,7 +51,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_officer = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     has_notification = models.IntegerField(default=0)
-    last_password_update = models.DateField(default="1980-01-01")
+    last_password_update = models.DateField(default="2015-01-01")
     phone = models.CharField(max_length=9, blank=True, null=True)
     objects = MyUserManager()
 
