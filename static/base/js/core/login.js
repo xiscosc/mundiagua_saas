924db92aa38984e92605d19003f0ff3d7c37c566
Xisco Sastre <xiscosastre@gmail.com>
2017-04-01 13:11:38 +0200

    login with enter

--- static/base/js/core/login.js
+++ static/base/js/core/login.js
@@ -2,10 +2,14 @@
  * Created by xiscosastre on 23/09/16.
  */
 
-$('#btn-log').on('click', function () {
-    $(this).html('<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Entrando...');
+
+$('form#form-log').on('submit', function (e) {
+    var self = this;
+    e.preventDefault();
+    $('#btn-log').html('<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Entrando...');
     $('#form-data').hide('slow', function () {
         $('#id_username').val($('#id_username').val().toLowerCase());
-        $('#form-log').submit();
+        self.submit();
     });
+    return false;
 });
