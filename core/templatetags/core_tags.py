8328b4cdd35a0aaa973fd3b11629d313bc5c2247
Francesc Sastre Cabot <xiscosastre@Xiscos-MacBook-Pro.local>
2017-07-14 19:09:36 +0200

    fix login background and static versions

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,4 +1,4 @@
-from random import randint
+import time
 from django import template
 from django.conf import settings
 
@@ -8,11 +8,20 @@ register = template.Library()
 @register.simple_tag
 def template_number():
     num = int(settings.NUMBER_TEMPLATES)
-    return str(randint(1, num))
+    day = int(time.strftime("%d"))
+    img_number = (day % num) + 1
+    return str(img_number)
+
 
 @register.simple_tag
 def app_version():
     return str(settings.APP_VERSION)
+
+
+@register.simple_tag
+def app_version_includes():
+    return "?v=" + str(settings.APP_VERSION_INCLUDES)
+
 
 @register.simple_tag
 def google_client_id():
