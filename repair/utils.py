4cd898577b18861bf372b8a16b090db9aeb18581
Francesc Sastre Cabot <francesc.sastrecabot@showpad.com>
2019-06-29 22:48:53 +0200

    6.5.3.1

--- repair/utils.py
+++ repair/utils.py
@@ -1,14 +1,15 @@
 from django.db.models import Count
+from django.db.models import Q
 
 
 def add_list_filters(repair_class, status, starred, budget):
 
     # Con presupuesto
     if budget == 1:
-        repairs = repair_class.objects.annotate(num_b=Count('budgetrepair')).filter(num_b__gt=0)
+        repairs = repair_class.objects.annotate(num_b=Count('budgetrepair')).filter(Q(num_b__gt=0) | Q(budget__isnull=False))
     # Sin presupuesto
     elif budget == 2:
-        repairs = repair_class.objects.annotate(num_b=Count('budgetrepair')).filter(num_b=0)
+        repairs = repair_class.objects.annotate(num_b=Count('budgetrepair')).filter(num_b=0, budget__isnull=True)
     else:
         repairs = repair_class.objects.all()
 
