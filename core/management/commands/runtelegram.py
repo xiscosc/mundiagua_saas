ce86153578f9fe11b39dbcb6c41823ffdfb5328d
Francesc Sastre Cabot <francesc.sastrecabot@showpad.com>
2020-10-19 20:20:32 +0200

    6.7.7

--- core/management/commands/runtelegram.py
+++ core/management/commands/runtelegram.py
@@ -3,47 +3,45 @@ from telegram.ext import Updater, CommandHandler
 from django.conf import settings
 
 
-def start(bot, update):
+def start(update, context):
     from core.models import User
     import telegram
     token = update.message.chat_id
-    bot.send_message(chat_id=token, text="Bienvenido a Mundiagua")
-    bot.send_chat_action(chat_id=token, action=telegram.ChatAction.TYPING)
+    update.message.reply_text("Bienvenido a Mundiagua")
+    update.send_chat_action(chat_id=token, action=telegram.ChatAction.TYPING)
     try:
         user = User.objects.get(telegram_token=token)
-        bot.send_message(chat_id=token, text="Bienvenido de nuevo " + user.first_name)
+        update.message.reply_text("Bienvenido de nuevo " + user.first_name)
     except Exception:
-        bot.send_message(chat_id=token,
-                         text="Este dispositvo no esta registrado, por favor registrelo para continuar, "
+        update.message.reply_text("Este dispositvo no esta registrado, por favor registrelo para continuar, "
                               "ejemplo: /register 123-1a2b3c4d5")
 
 
-def register(bot, update, args):
+def register(update, context):
     from core.models import User
     from core.utils import is_telegram_token
     import telegram
     token = update.message.chat_id
 
-    bot.send_chat_action(chat_id=token, action=telegram.ChatAction.TYPING)
+    update.send_chat_action(chat_id=token, action=telegram.ChatAction.TYPING)
     incorrect_token_message = "Token incorrecto, compruebe que su token es correcto. " \
                               "Recuerde que el token puede cambiar cada cierto tiempo."
-    if is_telegram_token(args[0]):
-        data = args[0].split('-')
+    if is_telegram_token(context.args[0]):
+        data = context.args[0].split('-')
         try:
             user = User.objects.get(id=int(data[0]))
             if user.get_telegram_auth() == data[1]:
                 if user.telegram_token:
-                    bot.send_message(chat_id=token,
-                                     text="Usted ya estaba registrado, se procedera a usar esta nueva cuenta")
+                    update.message.reply_text("Usted ya estaba registrado, se procedera a usar esta nueva cuenta")
                 user.telegram_token = token
                 user.save()
-                bot.send_message(chat_id=token, text="Registro completado. Bienvenido " + user.first_name)
+                update.message.reply_text("Registro completado. Bienvenido " + user.first_name)
             else:
-                bot.send_message(chat_id=token, text=incorrect_token_message)
+                update.message.reply_text(incorrect_token_message)
         except Exception:
-            bot.send_message(chat_id=token, text=incorrect_token_message)
+            update.message.reply_text(incorrect_token_message)
     else:
-        bot.send_message(chat_id=token, text=incorrect_token_message)
+        update.message.reply_text(incorrect_token_message)
 
 
 class Command(BaseCommand):
