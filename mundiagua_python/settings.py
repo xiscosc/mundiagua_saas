156b7394fbdd6cb33da031424d569da1489e603e
Francesc Sastre Cabot <francesc.sastrecabot@showpad.com>
2021-08-03 17:21:09 +0200

    fix url

--- mundiagua_python/settings.py
+++ mundiagua_python/settings.py
@@ -106,7 +106,7 @@ PUBLIC_URLS = (
     r'repair-status/(?P<online>\w+)/',
     r'clientes/',
     r'core/sms-gsm/notify',
-    r'client/whatsapp/file/download/(?P<key>[\w&.\-]+)/',
+    r'client/whatsapp/file/download/(?P<key>[\w&.\-]+)',
 
     # API URLS ACCESS WILL BE MANAGED IN ANOTHER PLACE
     r'api/*'
