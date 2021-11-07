c68e7f8ca708b78e235554adf4a8b7a8c1fe5a51
Francesc Sastre Cabot <francesc.sastrecabot@showpad.com>
2021-11-07 15:40:59 +0100

    add zodiac to admin

--- repair/admin.py
+++ repair/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from repair.models import AthRepair, IdegisRepair, AthRepairLog, IdegisRepairLog, RepairStatus
+from repair.models import AthRepair, IdegisRepair, ZodiacRepair, ZodiacRepairLog, AthRepairLog, IdegisRepairLog, \
+    RepairStatus
 
 admin.site.register(AthRepair)
 admin.site.register(AthRepairLog)
+admin.site.register(ZodiacRepair)
+admin.site.register(ZodiacRepairLog)
 admin.site.register(IdegisRepair)
 admin.site.register(IdegisRepairLog)
 admin.site.register(RepairStatus)
